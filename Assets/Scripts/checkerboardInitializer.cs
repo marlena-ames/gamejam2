2d0f8219857e84f9aa9911ffe16bf5ae5100df94
Alec Albright <alecalbright5@outlook.com>
2024-12-05 09:37:52 -0500

    Added Checkers Game

---
+++ Assets/Scripts/checkerboardInitializer.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class checkerboardInitializer : MonoBehaviour
+{
+    public static int dim = 8;
+    public GameObject checkerboard;
+    public static GameObject[,] g = new GameObject[dim, dim];
+    public static Vector2 Cmp;
+
+    private void Start()
+    {
+        Vector2 cs = transform.gameObject.GetComponent<RectTransform>().sizeDelta, size = checkerboard.GetComponent<RectTransform>().sizeDelta;
+        cs.x /= 2;
+        cs.y /= 2;
+        float left = (cs.x - size.x) * -1, top = (cs.y - size.y);
+        Color[] colors = new Color[] { Color.white, Color.black };
+        Image drt = checkerboard.GetComponent<Image>(), Ci = checkerboard.transform.Find("Pieces").GetComponent<Image>();
+
+        // Below is the loops for touching every panel on the board black and white one by one (nested loop)
+        for (int i = 0; i < dim; i++)
+        {
+            if (i % 2 == 0)
+            {
+                { colors[0] = Color.black; colors[1] = Color.white; }
+            }
+            else
+            {
+                { colors[0] = Color.white; colors[1] = Color.black; }
+            }
+
+            for(int j = 0; j < dim; j++)
+            {
+                drt.color = colors[(((j % 2) == 0) ? 0 : 1)];
+                if (i == (dim / 2) - 1 || i == (dim / 2) || drt.color == Color.white)
+                {
+                    Ci.enabled = false;
+                }
+                else
+                {
+                    Ci.enabled = true;
+                }
+
+                //Both of these conditions apply, if you want to edit piece colors change below
+                if (i < (dim / 2))
+                {
+                    Ci.color = Color.red;
+                }
+                else
+                {
+                    Ci.color = Color.white;
+                }
+                
+                if (drt.color == Color.white)
+                {
+                    checkerboard.transform.Find("k2").GetComponent<Image>().enabled = false;
+                }
+                else
+                {
+                    checkerboard.transform.Find("k2").GetComponent<Image>().enabled = true;
+                }
+
+                g[i, j] = Instantiate(checkerboard);
+                g[i, j].transform.SetParent(transform.Find("Panel1"));
+                g[i, j].transform.localPosition = new Vector3(left, top);
+                g[i, j].transform.name = i + "&" + j;
+                left += size.x;
+            }
+            left = (cs.x - size.x) * -1;
+            top -= size.y;
+        }
+    }
+    public static void OutlineSingleInstance()
+    {
+        // 8 is the size of the board could switch it to dim in other script
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                g[i, j].transform.Find("Outline").gameObject.GetComponent<Image>().enabled = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,132 @@
+using System;   
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class gameManager : MonoBehaviour
+{
+    static string k, k2 = "";
+    static Image Pieces;
+    static Color color = Color.white;
+    public GameObject Checkerboard;
+    public TMP_Text r, w;
+
+    public void Piecesclick(RectTransform t)
+    {
+        checkerboardInitializer.OutlineSingleInstance();
+        Pieces = t.transform.Find("Pieces").GetComponent<Image>();
+        if (color == Pieces.color)
+        {
+            string name = t.name;
+            int i, j;
+            i = Convert.ToInt32((name.Split("&"))[0]);
+            j = Convert.ToInt32((name.Split("&"))[1]);
+            int co = -1;
+
+            if (Pieces.color == Color.red)
+            {
+                co = 1;
+            }
+            try
+            {
+                if (!checkerboardInitializer.g[i + co, j - 1].transform.Find("Pieces").GetComponent<Image>().enabled)
+                {
+                    checkerboardInitializer.g[i + co, j - 1].transform.Find("Outline").GetComponent<Image>().enabled = true;
+                }
+                else if (checkerboardInitializer.g[i + co, j - 1].transform.Find("Pieces").GetComponent<Image>().color != Pieces.color && !checkerboardInitializer.g[i + (co * 2), j - 2].transform.Find("C").GetComponent<Image>().enabled)
+                {
+                    checkerboardInitializer.g[i + (co * 2), j - 2].transform.Find("Outline").GetComponent<Image>().enabled = true;
+                    k2 = (i + co) + " " + (j - 1);
+                }
+            }
+            catch { }
+            try
+            {
+                if (!checkerboardInitializer.g[i + co, j + 1].transform.Find("Pieces").GetComponent<Image>().enabled)
+                {
+                    checkerboardInitializer.g[i + co, j + 1].transform.Find("Outline").GetComponent<Image>().enabled = true;
+                }
+                else if (checkerboardInitializer.g[i + co, j + 1].transform.Find("Pieces").GetComponent<Image>().color != Pieces.color && !checkerboardInitializer.g[i + (co * 2), j + 2].transform.Find("C").GetComponent<Image>().enabled)
+                {
+                    checkerboardInitializer.g[i + (co * 2), j + 2].transform.Find("Outline").GetComponent<Image>().enabled = true;
+                    k2 = (i + co) + " " + (j + 1);
+                }
+            }
+            catch { }
+            k = i + " " + j;
+        }
+    }
+    public void A2(char c)
+    {
+        if (c == 'w')
+        {
+            checkerboardInitializer.Cmp.x++;
+        }
+        else
+        {
+            checkerboardInitializer.Cmp.y++;
+        }
+
+        //These if statements can be changed depending on color
+        if (checkerboardInitializer.Cmp.x >= 12)
+        {
+            Checkerboard.gameObject.SetActive(true);
+            Checkerboard.transform.Find("w").GetComponent<Text>().text = "White Wins!";
+        }
+
+        if (checkerboardInitializer.Cmp.y >= 12)
+        {
+            Checkerboard.gameObject.SetActive(true);
+            Checkerboard.transform.Find("w").GetComponent<Text>().text = "Red Wins!";
+        }
+    }
+
+    public void OutlineClick(RectTransform t)
+    {
+        checkerboardInitializer.OutlineSingleInstance();
+        string name = t.name;
+        int i, j;
+        i = Convert.ToInt32((name.Split('&'))[0]);
+        j = Convert.ToInt32((name.Split('&'))[1]);
+
+        checkerboardInitializer.g[i, j].transform.Find("Pieces").GetComponent<Image>().color = color;
+        checkerboardInitializer.g[i, j].transform.Find("Pieces").GetComponent<Image>().enabled = true;
+        
+        i = Convert.ToInt32((k.Split(' '))[0]);
+        j = Convert.ToInt32((k.Split(' '))[1]);
+
+        checkerboardInitializer.g[i, j].transform.Find("Pieces").GetComponent<Image>().enabled = false;
+        
+        if (k2 != "" && ((Convert.ToInt32((name.Split('&'))[1]) - 1) == Convert.ToInt32((k2.Split(' '))[1]) || (Convert.ToInt32((name.Split('&'))[1]) + 1) == Convert.ToInt32((k2.Split(' '))[1])))
+        {
+            i = Convert.ToInt32((k2.Split(' '))[0]);
+            j = Convert.ToInt32((k2.Split(' '))[1]);
+            checkerboardInitializer.g[i, j].transform.Find("Pieces").GetComponent<Image>().enabled = false;
+
+            if (color == Color.white)
+            {
+                A2('w');
+            }
+            else
+            {
+                A2('r');
+            }
+            r.text = checkerboardInitializer.Cmp.x + "";
+            w.text = checkerboardInitializer.Cmp.y + "";
+
+            k2 = "";
+        }
+
+        if (color == Color.white)
+        {
+            color = Color.red;
+        }
+        else
+        {
+            color = Color.white;
+        }
+    }
+}
